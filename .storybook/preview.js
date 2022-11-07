3669c38bb58c0d98ce2d752c8cbb6c06ddb0b495
Rafael Moro <rafael.moro.galindo@gmail.com>
2022-11-07 00:31:29 -0600

    [ADD] - configuration added in storybook for global styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,16 @@
+/** @jsxImportSource @emotion/react */
+import { Global } from '@emotion/react'
+import { GlobalStyles } from '../src/styles'
+
+const withGlobalProvider = (Story) => (
+  <>
+    <Global styles={GlobalStyles} />
+    <Story />
+  </>
+)
+
+export const decorators = [withGlobalProvider]
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
