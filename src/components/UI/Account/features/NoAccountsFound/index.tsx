c186270abea931053b8accfa0d72a0105a6ca615
Rafael Moro <rafael.moro.galindo@gmail.com>
2023-10-22 15:29:01 -0500

    [ADD] - Adding onClick callbacks into no accounts found and no records found button

--- src/components/UI/Account/features/NoAccountsFound/index.tsx
+++ src/components/UI/Account/features/NoAccountsFound/index.tsx
@@ -2,18 +2,25 @@ import { FlexContainer, Paragraph, PrimaryButton } from '../../../../../styles';
 import { NoAccountsFoundPicture } from './NoAccountsFound.styled';
 import noAccountsFoundPng from '../../../../../assets/no_accounts_found_png.png';
 import noAccountsFoundWebp from '../../../../../assets/no_accounts_found_webp.webp';
+import { useAccountsActions } from '../../../../../hooks/useAccountsActions';
 
-const NoAccountsFound = () => (
-  <FlexContainer gap="4" flexDirection="column" alignItems="center">
-    <NoAccountsFoundPicture>
-      <source srcSet={noAccountsFoundWebp} type="image/webp" />
-      <img src={noAccountsFoundPng} alt="Budget Master logo" />
-    </NoAccountsFoundPicture>
-    <Paragraph align="center">
-      You have not created accounts yet. Start now!
-    </Paragraph>
-    <PrimaryButton>Create account</PrimaryButton>
-  </FlexContainer>
-);
+const NoAccountsFound = () => {
+  const {
+    handleOpenCreateAccount,
+  } = useAccountsActions();
+
+  return (
+    <FlexContainer gap="4" flexDirection="column" alignItems="center">
+      <NoAccountsFoundPicture>
+        <source srcSet={noAccountsFoundWebp} type="image/webp" />
+        <img src={noAccountsFoundPng} alt="No Accounts Found" />
+      </NoAccountsFoundPicture>
+      <Paragraph align="center">
+        You have not created accounts yet. Start now!
+      </Paragraph>
+      <PrimaryButton onClick={handleOpenCreateAccount}>Create account</PrimaryButton>
+    </FlexContainer>
+  );
+};
 
 export { NoAccountsFound };
--- src/components/UI/Records/features/NoRecordsFound/index.tsx
+++ src/components/UI/Records/features/NoRecordsFound/index.tsx
@@ -1,20 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+
 import { FlexContainer, Paragraph, PrimaryButton } from '../../../../../styles';
 import { NotRecordsFoundPicture } from './NoRecordsFound.styled';
 import noRecordsFoundImgWebp from '../../../../../assets/no_records_found_webp.webp';
 import noRecordsFoundImgPng from '../../../../../assets/no_records_found_png.png';
+import { CREATE_RECORD_ROUTE } from '../../../../../pages/RoutesConstants';
 
 // Le falta el onclick para que abra la pagina de crear record
-const NoRecordsFound = () => (
-  <FlexContainer gap="2" flexDirection="column" alignItems="center">
-    <NotRecordsFoundPicture>
-      <source srcSet={noRecordsFoundImgWebp} type="image/webp" />
-      <img src={noRecordsFoundImgPng} alt="Budget Master logo" />
-    </NotRecordsFoundPicture>
-    <Paragraph align="center">
-      You have not created records for this account yet.
-    </Paragraph>
-    <PrimaryButton>Create Record</PrimaryButton>
-  </FlexContainer>
-);
+const NoRecordsFound = () => {
+  const navigate = useNavigate();
+  const navigateToCreateRecord = () => navigate(CREATE_RECORD_ROUTE);
+  return (
+    <FlexContainer gap="4" flexDirection="column" alignItems="center">
+      <NotRecordsFoundPicture>
+        <source srcSet={noRecordsFoundImgWebp} type="image/webp" />
+        <img src={noRecordsFoundImgPng} alt="No Records Found" />
+      </NotRecordsFoundPicture>
+      <Paragraph align="center">
+        You have not created records for this account yet.
+      </Paragraph>
+      <PrimaryButton onClick={navigateToCreateRecord}>Create Record</PrimaryButton>
+    </FlexContainer>
+  );
+};
 
 export { NoRecordsFound };
