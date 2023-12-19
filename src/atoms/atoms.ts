d0aaf93532461227ca3b63bcc9baccd7490fe618
Rafael Moro <rafael.moro.galindo@gmail.com>
2023-12-18 20:58:24 -0600

    [DELETE] - Delete total atom

--- src/atoms/atoms.ts
+++ src/atoms/atoms.ts
@@ -1,17 +1,6 @@
 import { atom } from 'jotai';
 import {
-  RecordsTotal, AnyRecord,
+  AnyRecord,
 } from '../globalInterface';
 
-export const initialTotalAtomState = {
-  currentMonth: {
-    expenseTotal: '$0.00',
-    incomeTotal: '$0.00',
-  },
-  lastMonth: {
-    expenseTotal: '$0.00',
-    incomeTotal: '$0.00',
-  },
-};
 export const recordToBeModifiedAtom = atom<AnyRecord | null>(null);
-export const totalAtom = atom<RecordsTotal>(initialTotalAtomState);
