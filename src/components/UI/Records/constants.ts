099239bcfe641da923ea352f3f31d8712a412438
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-01-18 21:35:44 -0600

    [FIX] - Fix bug on constant no expenses/income found

--- src/components/UI/Records/constants.ts
+++ src/components/UI/Records/constants.ts
@@ -5,4 +5,4 @@ export const EXPENSE_ROUTE = 'records/expenses';
 export const INCOME_ROUTE = 'records/incomes';
 export const UPDATE_MULTIPLE_EXPENSES = 'records/expenses/multiple';
 
-export const NO_EXPENSES_OR_INCOMES_FOUND = 'No incomes or expenses found';
+export const NO_EXPENSES_OR_INCOMES_FOUND = 'No incomes or expenses found.';
