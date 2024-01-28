7538a0ec6b97d19aee8b311dcd3b64ced4ee96d5
Jose Rafael Moro Galindo <rafael_moro_galindo@homedepot.com>
2024-01-28 06:52:45 -0600

    Fix borderSize default prop in loading spinner

--- src/components/UI/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/UI/LoadingSpinner/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ import { LoadingSpinnerProps } from './interface';
 import { AppColors } from '../../../styles';
 import { Spinner } from './LoadingSpinner.styled';
 
-const LoadingSpinner = ({ color = AppColors.white, borderSize = '0.5rem' }: LoadingSpinnerProps) => (
+const LoadingSpinner = ({ color = AppColors.white, borderSize = '0.5' }: LoadingSpinnerProps) => (
   <Spinner color={color} borderSize={borderSize} />
 );
 
