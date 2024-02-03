880e7bdcd0682e25bb385ff798a795d82b8e2261
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-02-03 08:27:52 -0600

    Change error icon brought from icons

--- src/components/UI/Error/Error.tsx
+++ src/components/UI/Error/Error.tsx
@@ -1,16 +1,13 @@
 import { Typography } from '@mui/material';
-import { ErrorOutlineOutlined } from '@mui/icons-material';
-import { AppColors } from '../../../styles';
 import { MessageContainer } from '../../../styles/LoginModule.styled';
 import { ErrorProps } from './interface';
-
-const { negative } = AppColors;
+import { ErrorIcon } from '../Icons';
 
 const Error = ({
   title, description, children, hideIcon = false,
 }: ErrorProps) => (
   <MessageContainer>
-    { !hideIcon && (<ErrorOutlineOutlined sx={{ fontSize: '4.5rem', fill: negative }} />) }
+    { !hideIcon && (<ErrorIcon size="4.5rem" />) }
     { title && (<Typography>{title}</Typography>) }
     <Typography>{description}</Typography>
     { children }
