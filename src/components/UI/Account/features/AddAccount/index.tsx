3d403df66cdd49edcb8823b4117e13ed7e671021
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-02-03 07:53:51 -0600

    Change Paragraph to Typography in create account card

--- src/components/UI/Account/features/AddAccount/index.tsx
+++ src/components/UI/Account/features/AddAccount/index.tsx
@@ -1,13 +1,14 @@
+import { Typography } from '@mui/material';
 import { AddOutlined } from '@mui/icons-material';
 
 import { AddAccountProps } from '../ViewAccounts/interface';
 import { AddAccountContainer } from '../../Account.styled';
-import { AppColors, Paragraph } from '../../../../../styles';
+import { AppColors } from '../../../../../styles';
 
 const AddAccount = ({ onClick }: AddAccountProps) => (
   <AddAccountContainer onClick={onClick}>
     <AddOutlined sx={{ fontSize: '4.5rem', fill: AppColors.primary }} />
-    <Paragraph>Create Account</Paragraph>
+    <Typography>Create Account</Typography>
   </AddAccountContainer>
 );
 
