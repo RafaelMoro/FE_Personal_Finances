98bd59efb706a8e042cd667c97ee6d9ee9c7faa6
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-02-03 08:52:06 -0600

    Replace for Typography

--- src/components/UI/Records/features/NoRecordsFound/NoRecordsFound.tsx
+++ src/components/UI/Records/features/NoRecordsFound/NoRecordsFound.tsx
@@ -1,13 +1,13 @@
 import { useNavigate } from 'react-router-dom';
+import { Typography } from '@mui/material';
 
-import { Paragraph, PrimaryButton } from '../../../../../styles';
+import { PrimaryButton } from '../../../../../styles';
 import { NotRecordsFoundPicture } from './NoRecordsFound.styled';
 import noRecordsFoundImgWebp from '../../../../../assets/no_records_found_webp.webp';
 import noRecordsFoundImgPng from '../../../../../assets/no_records_found_png.png';
 import { CREATE_RECORD_ROUTE } from '../../../../../pages/RoutesConstants';
 import { NoRecordsFoundContainer } from '../Features.styled';
 
-// Le falta el onclick para que abra la pagina de crear record
 const NoRecordsFound = () => {
   const navigate = useNavigate();
   const navigateToCreateRecord = () => navigate(CREATE_RECORD_ROUTE);
@@ -17,9 +17,9 @@ const NoRecordsFound = () => {
         <source srcSet={noRecordsFoundImgWebp} type="image/webp" />
         <img src={noRecordsFoundImgPng} alt="No Records Found" />
       </NotRecordsFoundPicture>
-      <Paragraph align="center">
+      <Typography align="center">
         You have not created records for this account yet.
-      </Paragraph>
+      </Typography>
       <PrimaryButton onClick={navigateToCreateRecord}>Create Record</PrimaryButton>
     </NoRecordsFoundContainer>
   );
