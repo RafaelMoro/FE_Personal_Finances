a4295166d1e87beecf8a0704bd5445b76d74175b
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-02-05 14:17:08 -0600

    Add record to be edited on local storage

--- src/utils/LocalStorage/interface.ts
+++ src/utils/LocalStorage/interface.ts
@@ -1,7 +1,8 @@
-import { User } from '../../globalInterface';
+import { AnyRecord, User } from '../../globalInterface';
 
 export interface CountOnMeLocalStorage {
   user: User;
+  recordToBeEdited: AnyRecord;
 }
 
 export interface JWT {
