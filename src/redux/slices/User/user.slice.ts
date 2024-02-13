93f3b1ebe90da7dc68c27329e83e88858b0ed7c1
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-02-13 09:59:44 -0600

    Delete async thunk and extra reducers in user slice

--- src/redux/slices/User/user.slice.ts
+++ src/redux/slices/User/user.slice.ts
@@ -1,12 +1,6 @@
 /* eslint-disable no-param-reassign */
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { postRequest, addToLocalStorage } from '../../../utils';
-import { LoginValues } from '../../../pages/LoginModule/Login/interface';
-import { OldUser } from '../../../globalInterface';
-import { LOGIN_POST_ROUTE } from '../../../pages/LoginModule/Login/constants';
-import { LoginResponse, UserInitialState } from './interface';
-import { forgotPasswordFulfilled, forgotPasswordPending, forgotPasswordRejected } from './actions/forgotPassword';
-import { resetPasswordFulfilled, resetPasswordPending, resetPasswordRejected } from './actions/resetPassword';
+import { createSlice } from '@reduxjs/toolkit';
+import { UserInitialState } from './interface';
 
 const userInitialState: UserInitialState = {
   userInfo: null,
@@ -20,21 +14,10 @@ const userInitialState: UserInitialState = {
   navigateToDashboard: false,
 };
 
-export const loginUser = createAsyncThunk(
-  'user/login',
-  async (values: LoginValues) => {
-    const loginResponse: LoginResponse = await postRequest(values, LOGIN_POST_ROUTE);
-    return loginResponse;
-  },
-);
-
 export const userSlice = createSlice({
   name: 'user',
   initialState: userInitialState,
   reducers: {
-    disableNavigateDashboardFlag: (state) => {
-      state.navigateToDashboard = false;
-    },
     signOff: (state) => {
       state.userInfo = null;
     },
@@ -45,48 +28,10 @@ export const userSlice = createSlice({
       state.successOnAction = false;
     },
   },
-  extraReducers: (builder) => {
-    builder.addCase(loginUser.pending, (state) => {
-      state.loading = true;
-
-      // Reset previous error status if it occurred
-      state.error = false;
-      state.errorMessage = '';
-    });
-
-    builder.addCase(loginUser.fulfilled, (state, action) => {
-      state.loading = false;
-
-      const { accessToken, user: { email } } = action.payload.data;
-      const bearerToken = { Authorization: `Bearer ${accessToken}` };
-      const user: OldUser = { accessToken, email, bearerToken };
-      addToLocalStorage(
-        {
-          user,
-        },
-      );
-      state.userInfo = user;
-      state.navigateToDashboard = true;
-    });
-
-    builder.addCase(loginUser.rejected, (state, action) => {
-      state.loading = false;
-      state.error = true;
-      state.errorMessage = action.error.message;
-    });
-
-    forgotPasswordPending(builder);
-    forgotPasswordFulfilled(builder);
-    forgotPasswordRejected(builder);
-
-    resetPasswordPending(builder);
-    resetPasswordFulfilled(builder);
-    resetPasswordRejected(builder);
-  },
 });
 
 export const {
-  disableNavigateDashboardFlag, signOff, signOn, resetSuccessOnAction,
+  signOff, signOn, resetSuccessOnAction,
 } = userSlice.actions;
 
 export default userSlice.reducer;
