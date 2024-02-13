b485d7999e947125e80202641df6555de74dbb20
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-02-13 09:27:42 -0600

    Change transform response with typing

--- src/redux/budgetMaster.api.ts
+++ src/redux/budgetMaster.api.ts
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BACKEND_ENV_URI, BACKEND_LOCAL_URI, POST_METHOD } from '../constants';
 import { LOGIN_POST_ROUTE } from '../pages/LoginModule/Login/constants';
+import { LoginResponse } from './slices/User/interface';
+import { User } from '../globalInterface';
 
 const BACKEND_URI = BACKEND_ENV_URI ?? BACKEND_LOCAL_URI;
 
@@ -15,6 +17,24 @@ export const budgetMasterApi = createApi({
         method: POST_METHOD,
         body: values,
       }),
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      transformResponse: (response: LoginResponse) => {
+        const { data } = response;
+        const {
+          accessToken, user: {
+            email, firstName, lastName, middleName,
+          },
+        } = data;
+        const bearerToken = `Bearer ${accessToken}`;
+        const user: User = {
+          bearerToken,
+          accessToken,
+          user: {
+            email, firstName, lastName, middleName,
+          },
+        };
+        return user;
+      },
     }),
   }),
 });
