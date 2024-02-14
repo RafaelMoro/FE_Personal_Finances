8eaeca78134c4c365ae1a7926684e3e435202426
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-02-13 21:13:22 -0600

    Update forgotPassword action to inject endpoint RTK query

--- src/redux/slices/User/actions/forgotPassword.ts
+++ src/redux/slices/User/actions/forgotPassword.ts
@@ -1,44 +1,17 @@
-/* eslint-disable no-param-reassign */
-import { ActionReducerMapBuilder, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { POST_METHOD } from '../../../../constants';
 import { FORGOT_PASSWORD_POST_ROUTE } from '../../../../pages/LoginModule/ForgotPassword/constants';
-import { GeneralResponse } from '../../../../globalInterface';
-import { ForgotPasswordValues } from '../../../../pages/LoginModule/ForgotPassword/interface';
-import { UserInitialState } from '../interface';
-import { BACKEND_ENV_URI } from '../../../../constants';
+import { budgetMasterApi } from '../../../budgetMaster.api';
 
-export const forgotPasswordThunkFn = createAsyncThunk(
-  'user/forgotPassword',
-  async (values: ForgotPasswordValues) => {
-    const standardResponse = await axios.post<GeneralResponse>(`${BACKEND_ENV_URI}/${FORGOT_PASSWORD_POST_ROUTE}`, values);
-    return standardResponse?.data;
-  },
-);
-
-export const forgotPasswordPending = (
-  builder: ActionReducerMapBuilder<UserInitialState>,
-) => builder.addCase(forgotPasswordThunkFn.pending, (state) => {
-  state.loadingOnAction = true;
-
-  // Reset previous error status if it occurred
-  state.errorOnAction = false;
-  state.successOnAction = false;
-  state.errorMessageOnAction = '';
+export const forgotPasswordApiSlice = budgetMasterApi.injectEndpoints({
+  endpoints: (builder) => ({
+    forgotPassword: builder.mutation({
+      query: ({ values }) => ({
+        url: FORGOT_PASSWORD_POST_ROUTE,
+        method: POST_METHOD,
+        body: values,
+      }),
+    }),
+  }),
 });
 
-export const forgotPasswordFulfilled = (
-  builder: ActionReducerMapBuilder<UserInitialState>,
-) => builder.addCase(forgotPasswordThunkFn.fulfilled, (state) => {
-  state.loadingOnAction = false;
-
-  state.successOnAction = true;
-});
-
-export const forgotPasswordRejected = (
-  builder: ActionReducerMapBuilder<UserInitialState>,
-) => builder.addCase(forgotPasswordThunkFn.rejected, (state, action) => {
-  state.loadingOnAction = false;
-  state.successOnAction = false;
-  state.errorOnAction = true;
-  state.errorMessageOnAction = action.error;
-});
+export const { useForgotPasswordMutation } = forgotPasswordApiSlice;
