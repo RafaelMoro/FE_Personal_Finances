7e7001b1a9f19d83f1ab08feb753e35afdf547db
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-02-14 14:04:29 -0600

    Delete unusued interfaces

--- src/redux/slices/Accounts/interface.ts
+++ src/redux/slices/Accounts/interface.ts
@@ -1,14 +1,9 @@
-import { AxiosRequestHeaders } from 'axios';
 import { AccountUI, CreateAccount, ModifyAccountValues } from '../../../components/UI/Account/interface';
 import { Account, GeneralResponse } from '../../../globalInterface';
 
 export interface AccountsInitialState {
   accounts: AccountUI[] | null;
   accountSelected: AccountUI | null;
-}
-
-export interface FetchAccountsValues {
-  bearerToken: AxiosRequestHeaders;
 }
 
 export interface DeleteAccountValues {
@@ -44,20 +39,4 @@ export interface FetchAccountsResponse extends Omit<GeneralResponse, 'data'> {
   data: {
     accounts: Account[]
   };
-}
-
-export interface SingleAccountResponse extends Omit<GeneralResponse, 'data'> {
-  data: {
-    account: Account;
-  };
-}
-
-export interface DeleteAccountData {
-  accountDeleted: Account;
-  numberExpensesDeleted: number;
-  numberIncomesDeleted: number;
-}
-
-export interface DeleteAccountResponse extends Omit<GeneralResponse, 'data'> {
-  data: DeleteAccountData;
 }
