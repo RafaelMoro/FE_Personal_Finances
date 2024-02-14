008dadda689e0ad0cb9122fe7bbe79b0fe1735cb
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-02-14 13:13:30 -0600

    Add account tag constant

--- src/redux/budgetMaster.api.ts
+++ src/redux/budgetMaster.api.ts
@@ -3,13 +3,14 @@ import { BACKEND_ENV_URI, BACKEND_LOCAL_URI, POST_METHOD } from '../constants';
 import { LOGIN_POST_ROUTE } from '../pages/LoginModule/Login/constants';
 import { LoginResponse } from './slices/User/interface';
 import { User } from '../globalInterface';
+import { ACCOUNT_TAG } from './constants';
 
 const BACKEND_URI = BACKEND_ENV_URI ?? BACKEND_LOCAL_URI;
 
 export const budgetMasterApi = createApi({
   reducerPath: 'budgetMasterApi',
   baseQuery: fetchBaseQuery({ baseUrl: BACKEND_URI }),
-  tagTypes: ['BudgetMaster'],
+  tagTypes: [ACCOUNT_TAG],
   endpoints: (builder) => ({
     login: builder.mutation({
       query: ({ values }) => ({
---
+++ src/redux/constants.ts
@@ -0,0 +1,4 @@
+export const ACCOUNT_TAG = 'Accounts';
+
+/** Fixed cached keys */
+export const LOGIN_FIXED_CACHED_KEY = 'login-info';
