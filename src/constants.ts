cb0a6785b65f9a3de53f3fb83046fcb17ca0af2a
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-02-13 21:39:49 -0600

    Update constants

--- src/constants.ts
+++ src/constants.ts
@@ -6,12 +6,19 @@ export const POST_METHOD = 'POST';
 
 /** Errors */
 export const NETWORK_CATCH_ERROR = 'Network Error';
-export const NETWORK_ERROR_MESSAGE = 'There is a network error. Please check you are connected to Internet.';
-export const UNAUTHORIZED_ERROR = 'Email or Password incorrect.';
-export const ERROR_MESSAGE_UNAUTHORIZED = 'Email or Password incorrect.';
+export const JWT_EXPIRED_CATCH_ERROR = 'jwt expired';
+
 export const ERROR_TITLE_GENERAL = 'Error';
 export const ERROR_MESSAGE_GENERAL = 'Oops! Something went wrong. Try again later.';
-export const ERROR_USER_NOT_FOUND = 'User not found.';
+export const NETWORK_ERROR_MESSAGE = 'There is a network error. Please check you are connected to Internet.';
+
+/** Errors Login Module */
+export const USER_NOT_FOUND_CATCH_ERROR = 'User not found.';
+export const TOKEN_EXPIRED_TITLE = 'Your token to reset your password has expired';
+export const TOKEN_EXPIRED_DESC = 'Redirecting you to forgot password to try again.';
+export const UNAUTHORIZED_ERROR = 'Email or Password incorrect.';
+export const ERROR_MESSAGE_UNAUTHORIZED = 'Email or Password incorrect.';
+
 export const ERROR_MESSAGE_FETCH_CATEGORIES = 'We could not get your categories. Please try again later';
 export const ERROR_INCORRECT_MAIL_DESC = 'Verify that your email is correct or create an account.';
 
