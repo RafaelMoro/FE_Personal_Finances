0cfb96b0d4f6cea8d5c85f642ae32354fc334ce7
Jose Rafael Moro Galindo <rafael_moro_galindo@homedepot.com>
2024-02-16 18:12:33 -0600

    Delete extra reducers and fetch categories flags Create setAllCategories to set the categories state

--- src/redux/slices/Categories/categories.slice.ts
+++ src/redux/slices/Categories/categories.slice.ts
@@ -1,21 +1,24 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
 import { CategoriesInitialState } from './interface';
-import { fetchCategoriesFulfilled, fetchCategoriesPending, fetchCategoriesRejected } from './actions/fetchCategories';
+import { CATEGORIES_RECORDS } from '../../../constants';
 
 const categoriesInitialState: CategoriesInitialState = {
   categories: null,
   currentCategory: null,
   categoryNotSelected: true,
-  loading: false,
-  error: false,
-  errorMessage: '',
 };
 
 export const categoriesSlice = createSlice({
   name: 'categories',
   initialState: categoriesInitialState,
   reducers: {
+    setAllCategories: (state, action) => {
+      const allCategories = [...CATEGORIES_RECORDS, ...action.payload];
+      const categoriesSet = new Set(allCategories);
+      const uniqueCategories = [...categoriesSet];
+      state.categories = uniqueCategories;
+    },
     updateCurrentCategory: (state, action) => {
       state.currentCategory = action.payload;
     },
@@ -23,13 +26,8 @@ export const categoriesSlice = createSlice({
       state.categoryNotSelected = false;
     },
   },
-  extraReducers: (builder) => {
-    fetchCategoriesFulfilled(builder);
-    fetchCategoriesPending(builder);
-    fetchCategoriesRejected(builder);
-  },
 });
 
-export const { updateCurrentCategory, isCategorySelected } = categoriesSlice.actions;
+export const { updateCurrentCategory, isCategorySelected, setAllCategories } = categoriesSlice.actions;
 
 export default categoriesSlice.reducer;
