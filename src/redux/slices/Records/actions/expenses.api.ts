054c36a2229fc6d5897f3dc67b08b4f54e573a38
Jose Rafael Moro Galindo <rafael_moro_galindo@homedepot.com>
2024-02-17 08:52:33 -0600

    Add update paid multiple expenses rtk mutation

--- src/redux/slices/Records/actions/expenses.api.ts
+++ src/redux/slices/Records/actions/expenses.api.ts
@@ -1,4 +1,4 @@
-import { EXPENSE_ROUTE } from '../../../../components/UI/Records/constants';
+import { EXPENSE_ROUTE, UPDATE_MULTIPLE_EXPENSES } from '../../../../components/UI/Records/constants';
 import { POST_METHOD, PUT_METHOD } from '../../../../constants';
 import { budgetMasterApi } from '../../../budgetMaster.api';
 import { RECORD_TAG } from '../../../constants';
@@ -29,7 +29,18 @@ export const expensesApiSlice = budgetMasterApi.injectEndpoints({
       }),
       invalidatesTags: [RECORD_TAG],
     }),
+
+    updatePaidMultipleExpenses: builder.mutation({
+      query: ({ values, bearerToken }) => ({
+        url: UPDATE_MULTIPLE_EXPENSES,
+        method: PUT_METHOD,
+        body: values,
+        headers: {
+          Authorization: bearerToken,
+        },
+      }),
+    }),
   }),
 });
 
-export const { useCreateExpenseMutation, useEditExpenseMutation } = expensesApiSlice;
+export const { useCreateExpenseMutation, useEditExpenseMutation, useUpdatePaidMultipleExpensesMutation } = expensesApiSlice;
