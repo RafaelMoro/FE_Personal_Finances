c601660e95be101d7fd96f3c3c3e03f2783b2a4a
Jose Rafael Moro Galindo <rafael_moro_galindo@homedepot.com>
2024-02-17 08:48:49 -0600

    Add edit income rtk mutation

--- src/redux/slices/Records/incomes.api.ts
+++ src/redux/slices/Records/incomes.api.ts
@@ -1,5 +1,5 @@
 import { INCOME_ROUTE } from '../../../components/UI/Records/constants';
-import { POST_METHOD } from '../../../constants';
+import { POST_METHOD, PUT_METHOD } from '../../../constants';
 import { budgetMasterApi } from '../../budgetMaster.api';
 import { RECORD_TAG } from '../../constants';
 
@@ -16,7 +16,19 @@ export const incomesApiSlice = budgetMasterApi.injectEndpoints({
       }),
       invalidatesTags: [RECORD_TAG],
     }),
+
+    editIncome: builder.mutation({
+      query: ({ values, bearerToken }) => ({
+        url: INCOME_ROUTE,
+        method: PUT_METHOD,
+        body: values,
+        headers: {
+          Authorization: bearerToken,
+        },
+      }),
+      invalidatesTags: [RECORD_TAG],
+    }),
   }),
 });
 
-export const { useCreateIncomeMutation } = incomesApiSlice;
+export const { useCreateIncomeMutation, useEditIncomeMutation } = incomesApiSlice;
