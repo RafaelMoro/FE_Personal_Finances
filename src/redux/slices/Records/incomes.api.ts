4c293ea912636124d369c377a18cef72ce820238
Jose Rafael Moro Galindo <rafael_moro_galindo@homedepot.com>
2024-02-16 19:06:52 -0600

    Create income api slice

---
+++ src/redux/slices/Records/incomes.api.ts
@@ -0,0 +1,22 @@
+import { INCOME_ROUTE } from '../../../components/UI/Records/constants';
+import { POST_METHOD } from '../../../constants';
+import { budgetMasterApi } from '../../budgetMaster.api';
+import { RECORD_TAG } from '../../constants';
+
+export const incomesApiSlice = budgetMasterApi.injectEndpoints({
+  endpoints: (builder) => ({
+    createIncome: builder.mutation({
+      query: ({ values, bearerToken }) => ({
+        url: INCOME_ROUTE,
+        method: POST_METHOD,
+        body: values,
+        headers: {
+          Authorization: bearerToken,
+        },
+      }),
+      invalidatesTags: [RECORD_TAG],
+    }),
+  }),
+});
+
+export const { useCreateIncomeMutation } = incomesApiSlice;
