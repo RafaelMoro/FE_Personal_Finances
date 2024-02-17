d11e2da32160952039f4c6b7e2de83d445a301d5
Jose Rafael Moro Galindo <rafael_moro_galindo@homedepot.com>
2024-02-16 18:46:20 -0600

    Add invalidate tag of create expense, add post method

--- src/redux/slices/Records/actions/expenses.api.ts
+++ src/redux/slices/Records/actions/expenses.api.ts
@@ -1,5 +1,7 @@
 import { EXPENSE_ROUTE } from '../../../../components/UI/Records/constants';
+import { POST_METHOD } from '../../../../constants';
 import { budgetMasterApi } from '../../../budgetMaster.api';
+import { RECORD_TAG } from '../../../constants';
 import { CreateExpenseMutationProps } from '../interface';
 
 export const expensesApiSlice = budgetMasterApi.injectEndpoints({
@@ -7,11 +9,13 @@ export const expensesApiSlice = budgetMasterApi.injectEndpoints({
     createExpense: builder.mutation({
       query: ({ values, bearerToken }: CreateExpenseMutationProps) => ({
         url: EXPENSE_ROUTE,
+        method: POST_METHOD,
         body: values,
         headers: {
           Authorization: bearerToken,
         },
       }),
+      invalidatesTags: [RECORD_TAG],
     }),
   }),
 });
