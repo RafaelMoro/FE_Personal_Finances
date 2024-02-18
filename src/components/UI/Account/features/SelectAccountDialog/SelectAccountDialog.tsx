0e7bc8456924ee8221115bc4d5b7fbd00b44af5d
Jose Rafael Moro Galindo <rafael_moro_galindo@homedepot.com>
2024-02-18 09:05:56 -0600

    Delete unused code and imports from select account dialog

--- src/components/UI/Account/features/SelectAccountDialog/SelectAccountDialog.tsx
+++ src/components/UI/Account/features/SelectAccountDialog/SelectAccountDialog.tsx
@@ -2,10 +2,8 @@ import {
   Dialog, List, ListItem as ListAccount, Divider,
 } from '@mui/material';
 
-import { GET_EXPENSES_AND_INCOMES_BY_MONTH_ROUTE } from '../../../Records/constants';
 import { AccountUI } from '../../interface';
 import { AccountDialogProps } from './interface';
-import { useDate } from '../../../../../hooks/useDate';
 import { useAppDispatch, useAppSelector } from '../../../../../redux/hooks';
 import { updateSelectedAccount, updateAccounts } from '../../../../../redux/slices/Accounts/accounts.slice';
 import { ListAccountSelected, ListItemButtonContainer } from './SelectAccountDialog.styled';
@@ -17,9 +15,6 @@ const SelectAccountDialog = ({
   const dispatch = useAppDispatch();
   const accountsReduxState = useAppSelector((state) => state.accounts);
   const accountsUI = accountsReduxState?.accounts;
-  const user = useAppSelector((state) => state.user);
-  const bearerToken = user.userInfo?.bearerToken as string;
-  const { month, year } = useDate();
 
   const handleAccountClick = (accountId: string) => {
     if (accountsUI) {
@@ -36,11 +31,6 @@ const SelectAccountDialog = ({
         };
       });
       dispatch(updateAccounts(newAccounts));
-
-      // Fetch records of selected account
-      const expensesFullRoute = `${GET_EXPENSES_AND_INCOMES_BY_MONTH_ROUTE}/${accountId}/${month}/${year}`;
-      // dispatch(fetchCurrentMonthRecords({ recordsFullRoute: expensesFullRoute, bearerToken }));
-
       onClose();
     }
   };
