f9f125eb8d604eba80bcdd0ed488427d545134ec
Jose Rafael Moro Galindo <rafael_moro_galindo@homedepot.com>
2024-02-18 08:06:38 -0600

    Add reducer to update the amount of the selected account

--- src/redux/slices/Accounts/accounts.slice.ts
+++ src/redux/slices/Accounts/accounts.slice.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
 import { AccountsInitialState, UpdateAccountsStatusProps } from './interface';
+import { formatNumberToCurrency } from '../../../utils';
 
 const accountsInitialState: AccountsInitialState = {
   accounts: null,
@@ -23,6 +24,27 @@ export const accountsSlice = createSlice({
 
       state.accountsFetchStatus = 'success';
     },
+    updateAmountSelectedAccount: (state, action) => {
+      const amountNumber = action.payload;
+      const amountFormatted = formatNumberToCurrency(amountNumber);
+
+      // Update the amount of the selected account
+      if (state.accountSelected) {
+        state.accountSelected.amount = amountNumber;
+        state.accountSelected.amountFormatted = amountFormatted;
+      }
+
+      // Update the account in the accounts state
+      if (state.accounts && state.accountSelected) {
+        const accountsUpdated = state.accounts.map((account) => {
+          if (account._id === state.accountSelected?._id) {
+            return { ...account, amount: amountNumber, amountFormatted };
+          }
+          return account;
+        });
+        state.accounts = accountsUpdated;
+      }
+    },
     updateSelectedAccount: (state, action) => {
       state.accountSelected = action.payload;
     },
@@ -36,7 +58,8 @@ export const accountsSlice = createSlice({
 });
 
 export const {
-  updateAccounts, updateSelectedAccount, resetAccounts, resetSelectedAccount, updateAccountsStatus,
+  updateAccounts, updateSelectedAccount, resetAccounts,
+  resetSelectedAccount, updateAccountsStatus, updateAmountSelectedAccount,
 } = accountsSlice.actions;
 
 export default accountsSlice.reducer;
