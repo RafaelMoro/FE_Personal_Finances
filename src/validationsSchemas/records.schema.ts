9df0bfa05b4f052e297ed3f10b8e04a57667b091
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-02-24 09:34:22 -0600

    Change verbiage validation error on short description field

--- src/validationsSchemas/records.schema.ts
+++ src/validationsSchemas/records.schema.ts
@@ -15,7 +15,7 @@ export const IndebtedPeopleFormSchema = Yup.object({
 });
 
 export const CreateRecordSchema = Yup.object({
-  shortName: stringRequired('Short name is required'),
+  shortName: stringRequired('Short description is required'),
   category: stringRequired('Category is required'),
   subCategory: stringRequired('Subcategory is required'),
   amount: stringRequired('Amount is required'),
