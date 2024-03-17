e342c07dc5c032878a61de35d6ef6fa9e1d1601d
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-03-17 10:43:08 -0600

    Pass login tests that include reroute to a different test suite because history location is not getting reset

--- src/pages/LoginModule/Login/Login.test.tsx
+++ src/pages/LoginModule/Login/Login.test.tsx
@@ -8,6 +8,7 @@ import fetchMock from 'jest-fetch-mock';
 
 import { Login } from './Login';
 import { WrapperRedux } from '../../../tests/WrapperRedux';
+import { DASHBOARD_ROUTE, REGISTER_ROUTE } from '../../RoutesConstants';
 
 const credentials = {
   email: 'example@mail.com',
@@ -34,6 +35,7 @@ const successfulLoginResponse = {
 describe('<Login />', () => {
   const history: MemoryHistory = createMemoryHistory();
   beforeEach(() => {
+    fetchMock.resetMocks();
     render(
       <WrapperRedux>
         <Router location={history.location} navigator={history}>
@@ -80,37 +82,6 @@ describe('<Login />', () => {
     });
   });
 
-  test('Click the button Register and redirect into the register route', async () => {
-    const registerLink = screen.getByRole('link', { name: /register/i });
-
-    fireEvent.click(registerLink);
-
-    await waitFor(() => {
-      expect(history.location.pathname).toBe('/register');
-    }, {
-      timeout: 4000,
-    });
-  });
-
-  test('Submit the form and have a successful return', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify(successfulLoginResponse));
-    const emailInput = screen.getByRole('textbox', { name: /email/i });
-    const passwordInput = screen.getByLabelText(/password/i);
-    const loginButton = screen.getByRole('button', { name: /login/i });
-
-    // Mock the successful response
-
-    userEvent.type(emailInput, credentials.email);
-    userEvent.type(passwordInput, credentials.password);
-    userEvent.click(loginButton);
-
-    await waitFor(() => {
-      // expect the mock to be called.
-      expect(fetchMock).toHaveBeenCalled();
-      // expect(fetchMock.mock.calls).toContainEqual(successfulLoginResponse);
-    });
-  });
-
   test.skip('Submit the form and have a unsuccessful return', async () => {
     const emailInput = screen.getByRole('textbox', { name: /email/i });
     const passwordInput = screen.getByLabelText(/password/i);
@@ -141,6 +112,61 @@ describe('<Login />', () => {
     await waitFor(() => {
       const errorNotification = screen.getByRole('heading', { name: /error/i });
       expect(errorNotification).toBeInTheDocument();
+    });
+  });
+});
+
+describe('Test Login re routes', () => {
+  beforeEach(() => {
+    fetchMock.resetMocks();
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+  test('Click the button Register and redirect into the register route', async () => {
+    const history: MemoryHistory = createMemoryHistory();
+    render(
+      <WrapperRedux>
+        <Router location={history.location} navigator={history}>
+          <Login />
+        </Router>
+      </WrapperRedux>,
+    );
+    const registerLink = screen.getByRole('link', { name: /register/i });
+
+    fireEvent.click(registerLink);
+
+    await waitFor(() => {
+      expect(history.location.pathname).toBe(REGISTER_ROUTE);
+    }, {
+      timeout: 4000,
+    });
+  });
+
+  test('Submit the form and have a successful return', async () => {
+    const history: MemoryHistory = createMemoryHistory();
+    fetchMock.mockResponseOnce(JSON.stringify(successfulLoginResponse));
+    render(
+      <WrapperRedux>
+        <Router location={history.location} navigator={history}>
+          <Login />
+        </Router>
+      </WrapperRedux>,
+    );
+    const emailInput = screen.getByRole('textbox', { name: /email/i });
+    const passwordInput = screen.getByLabelText(/password/i);
+    const loginButton = screen.getByRole('button', { name: /login/i });
+
+    userEvent.type(emailInput, credentials.email);
+    userEvent.type(passwordInput, credentials.password);
+    userEvent.click(loginButton);
+
+    await waitFor(() => {
+      expect(fetchMock).toHaveBeenCalled();
+      // expect(fetchMock.mock.calls).toContainEqual(successfulLoginResponse);
+    });
+    await waitFor(() => {
+      expect(history.location.pathname).toBe(DASHBOARD_ROUTE);
+    }, {
+      timeout: 4000,
     });
   });
 });
