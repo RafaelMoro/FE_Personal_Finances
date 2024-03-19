d23e5e1fc5ff665c290bf2d614275a2454112283
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-03-18 23:57:54 -0600

    Add rootReducer and setupStore for testing

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/User/user.slice';
 import accountsReducer from './slices/Accounts/accounts.slice';
 import userInterfaceReducer from './slices/userInterface.slice';
@@ -18,5 +18,20 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(budgetMasterApi.middleware),
 });
 
+// Add rootReducer and setupStore for testing.
+const rootReducer = combineReducers({
+  user: userReducer,
+  accounts: accountsReducer,
+  records: recordsReducer,
+  categories: categoriesReducer,
+  userInterface: userInterfaceReducer,
+});
+
+export const setupStore = (preloadedState?: Partial<RootState>) => configureStore({
+  reducer: rootReducer,
+  preloadedState,
+});
+
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
+export type AppStore = ReturnType<typeof setupStore>;
