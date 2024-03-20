e8aac5b057e12272b53dcde6978893a6e8875cc3
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-03-20 06:09:46 -0600

    Add TransferAccountSelector component

---
+++ src/components/UI/Records/features/TransferAccountSelector/TransferAccountSelector.tsx
@@ -0,0 +1,29 @@
+import { useAppSelector } from '../../../../../redux/hooks';
+import { SelectInput } from '../../../SelectInput';
+
+const TransferAccountSelector = () => {
+  const accounts = useAppSelector((state) => state.accounts.accounts);
+  const accountsOptions = (accounts ?? []).map((account) => account.title);
+  return (
+    <>
+      <SelectInput
+        labelId="select-origin-account"
+        labelName="Origin Account"
+        fieldName="origin-account"
+        stringOptions={accountsOptions}
+        colorOptions={[]}
+        processSelectDataFn={() => {}}
+      />
+      <SelectInput
+        labelId="select-destiny-account"
+        labelName="Destiny Account"
+        fieldName="destiny-account"
+        stringOptions={accountsOptions}
+        colorOptions={[]}
+        processSelectDataFn={() => {}}
+      />
+    </>
+  );
+};
+
+export { TransferAccountSelector };
