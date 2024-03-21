67f65a7a31cf01ca7ecf685d7100bf6d0265e4da
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-03-20 22:45:47 -0600

    Add show expenses and select expenses into Transfer

--- src/components/UI/Records/features/Transfer/Transfer.tsx
+++ src/components/UI/Records/features/Transfer/Transfer.tsx
@@ -2,6 +2,7 @@ import dayjs from 'dayjs';
 import { useState } from 'react';
 import { Formik } from 'formik';
 
+import { Drawer } from '@mui/material';
 import { DASHBOARD_ROUTE } from '../../../../../pages/RoutesConstants';
 import { useAppSelector } from '../../../../../redux/hooks';
 import { AccountUI } from '../../../Account/interface';
@@ -13,8 +14,13 @@ import { scrollToTop } from '../../../../../utils/ScrollToTop';
 import { TransferAccountSelector } from '../TransferAccountSelector';
 import { TransactionFormFields } from '../TransactionFormFields';
 import { ActionButtonPanel } from '../../../../templates';
-import { FormContainer } from '../RecordTemplate/RecordTemplate.styled';
+import { FormContainer, SecondaryButtonForm } from '../RecordTemplate/RecordTemplate.styled';
 import { useRecords } from '../../../../../hooks/useRecords';
+// Reuse imports on RecordTemplate
+import { ShowExpenses } from '../ShowExpenses';
+import { FlexContainer } from '../../../../../styles';
+import { ExpensePaid } from '../../../../../globalInterface';
+import { SelectExpenses } from '../SelectExpenses';
 
 interface TransferProps {
   action: string;
@@ -31,6 +37,9 @@ const Transfer = ({ action, typeOfRecord }: TransferProps) => {
   } = useRecords({});
   const selectedAccount = useAppSelector((state) => state.accounts.accountSelected);
   const [isCreditDestinationAcc, setIsCreditDestinationAcc] = useState<boolean>(false);
+  // Reuse show expenses and expenses selected state
+  const [expensesSelected, setExpensesSelected] = useState<ExpensePaid[]>([]);
+  const [showExpenses, setShowExpenses] = useState<boolean>(false);
   const [initialValues, setInitialValues] = useState<CreateTransferValues>({
     originAccount: (selectedAccount as AccountUI)._id,
     destinationAccount: '',
@@ -46,8 +55,19 @@ const Transfer = ({ action, typeOfRecord }: TransferProps) => {
     tag: [],
   });
 
+  const showExpenseText = expensesSelected.length === 0 ? 'Add Expense' : 'Add or Remove Expense';
+
   const setDestinationAsCredit = () => setIsCreditDestinationAcc(true);
   const setDestinationAsNonCredit = () => setIsCreditDestinationAcc(false);
+  // code repeated from RecordTemplate
+  // show expenses and expenses selected state
+  const closeShowExpenses = () => setShowExpenses(false);
+  const addExpenseToIncome = (expenses: ExpensePaid[]) => setExpensesSelected(expenses);
+  const toggleShowExpenses = (values: CreateTransferValues) => {
+    // save initial values
+    setInitialValues(values);
+    setShowExpenses(!showExpenses);
+  };
 
   const updateTags = (newChips: string[]) => {
     setInitialValues({ ...initialValues, tag: newChips });
@@ -85,61 +105,77 @@ const Transfer = ({ action, typeOfRecord }: TransferProps) => {
     createTransfer({ valuesExpense: newValuesExpense, valuesIncome: newValuesIncome });
   };
 
+  // reuse drawer of select expenses and show expenses components
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={(values) => handleSubmit(values)}
-      validationSchema={TransferSchema}
-      enableReinitialize
-      validateOnMount
-    >
-      {({
-        submitForm, errors, touched, setFieldValue,
-      }) => {
-        const hasErrors = Object.keys(errors).length > 0;
-        return (
-          <FormContainer>
-            <TransferAccountSelector
-              errorDestinationAccount={errors.destinationAccount}
-              errorOriginAccount={errors.originAccount}
-              touchedDestinationAccount={touched.destinationAccount}
-              touchedOriginAccount={touched.originAccount}
-              setDestinationAsCredit={setDestinationAsCredit}
-              setDestinationAsNonCredit={setDestinationAsNonCredit}
-            />
-            <TransactionFormFields
-              typeOfRecord={typeOfRecord}
-              setFieldValue={setFieldValue}
-              errors={errors}
-              touched={touched}
+    <>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => handleSubmit(values)}
+        validationSchema={TransferSchema}
+        enableReinitialize
+        validateOnMount
+      >
+        {({
+          submitForm, errors, touched, setFieldValue, values,
+        }) => {
+          const hasErrors = Object.keys(errors).length > 0;
+          return (
+            <FormContainer>
+              <TransferAccountSelector
+                errorDestinationAccount={errors.destinationAccount}
+                errorOriginAccount={errors.originAccount}
+                touchedDestinationAccount={touched.destinationAccount}
+                touchedOriginAccount={touched.originAccount}
+                setDestinationAsCredit={setDestinationAsCredit}
+                setDestinationAsNonCredit={setDestinationAsNonCredit}
+              />
+              <TransactionFormFields
+                typeOfRecord={typeOfRecord}
+                setFieldValue={setFieldValue}
+                errors={errors}
+                touched={touched}
               // @TODO Change this to the real value.
-              categoryToBeEdited={null}
-              updateBudgets={updateBudgets}
-              updateTags={updateTags}
-              tags={initialValues.tag}
-              budgets={initialValues.budgets}
-            />
-            <ActionButtonPanel
-              routeCancelButton={DASHBOARD_ROUTE}
-              minWidthNumber="18"
-              actionDataTestId="create-edit-transfer-button"
-              submitButtonText={buttonText}
-              loading={loadingMutation}
-              success={successMutation}
-              disableSubmitButton={loadingMutation || successMutation}
-              submitForm={() => {
-                if (hasErrors) {
-                  scrollToTop();
+                categoryToBeEdited={null}
+                updateBudgets={updateBudgets}
+                updateTags={updateTags}
+                tags={initialValues.tag}
+                budgets={initialValues.budgets}
+              />
+              { (isCreditDestinationAcc) && (
+              <>
+                <ShowExpenses usePagination expenses={expensesSelected} />
+                <FlexContainer justifyContent="center">
+                  <SecondaryButtonForm variant="contained" onClick={() => toggleShowExpenses(values)} size="medium">
+                    {showExpenseText}
+                  </SecondaryButtonForm>
+                </FlexContainer>
+              </>
+              ) }
+              <ActionButtonPanel
+                routeCancelButton={DASHBOARD_ROUTE}
+                minWidthNumber="18"
+                actionDataTestId="create-edit-transfer-button"
+                submitButtonText={buttonText}
+                loading={loadingMutation}
+                success={successMutation}
+                disableSubmitButton={loadingMutation || successMutation}
+                submitForm={() => {
+                  if (hasErrors) {
+                    scrollToTop();
+                    submitForm();
+                    return;
+                  }
                   submitForm();
-                  return;
-                }
-                submitForm();
-              }}
-            />
-          </FormContainer>
-        );
-      }}
-    </Formik>
+                }}
+              />
+            </FormContainer>
+          );
+        }}
+      </Formik>
+      <Drawer anchor="right" open={showExpenses} onClose={closeShowExpenses}>
+        <SelectExpenses modifySelectedExpenses={addExpenseToIncome} selectedExpenses={expensesSelected} closeDrawer={closeShowExpenses} />
+      </Drawer>
+    </>
   );
 };
 
