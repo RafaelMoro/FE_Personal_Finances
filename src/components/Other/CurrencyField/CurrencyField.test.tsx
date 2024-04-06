462ac555cb5d40f10bd6fa24bd82e052380506e5
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-04-06 10:12:52 -0600

    Fix test on render amount field

--- src/components/Other/CurrencyField/CurrencyField.test.tsx
+++ src/components/Other/CurrencyField/CurrencyField.test.tsx
@@ -1,19 +1,33 @@
 import { render, screen } from '@testing-library/react';
+import { Formik } from 'formik';
+import { CreateRecordSchema } from '../../../validationsSchemas/records.schema';
 import { CurrencyField } from './CurrencyField';
 import { TypeOfRecord } from '../../../globalInterface';
 
 describe('CurrencyField component', () => {
   const amount = '0';
+  const initialValues = { amount: '' };
   const typeOfRecord: TypeOfRecord = 'income';
   const setFieldValue = jest.fn();
   const updateAmount = jest.fn();
+  const handleSubmit = jest.fn();
   test('Show an amount field with Amount placeholder', () => {
-    render(<CurrencyField
-      amount={amount}
-      typeOfRecord={typeOfRecord}
-      setFieldValue={setFieldValue}
-      updateAmount={updateAmount}
-    />);
+    render(
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={CreateRecordSchema}
+        enableReinitialize
+        validateOnMount
+      >
+        <CurrencyField
+          amount={amount}
+          typeOfRecord={typeOfRecord}
+          setFieldValue={setFieldValue}
+          updateAmount={updateAmount}
+        />
+      </Formik>,
+    );
     const input = screen.getByRole('textbox', { name: /amount/i });
     expect(input).toBeInTheDocument();
   });
