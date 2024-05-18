e9696683598e9a321208592e60bba2faaff330c9
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-05-18 10:11:55 -0600

    Fix hero styles on landing page

--- src/pages/LandingPage/LandingPage.styled.tsx
+++ src/pages/LandingPage/LandingPage.styled.tsx
@@ -1,22 +1,35 @@
 import styled from '@emotion/styled';
-import { responsiveBreakpoints } from '../../styles';
+import { Typography } from '@mui/material';
+import { AppColors, responsiveBreakpoints } from '../../styles';
 import heroDesktop from '../../assets/hero-desktop-webp.webp';
 import heroMobile from '../../assets/hero-mobile-webp.webp';
 import heroTablet from '../../assets/hero-tablet-webp.webp';
 
 export const Hero = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  display: grid;
+  place-items: center;
+  height: 50rem;
   background-image: url(${heroMobile});
+  background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${heroMobile});
   background-repeat:no-repeat;
-  background-size:contain;
+  background-size: cover;
+  background-position: center center;
 
   @media ${responsiveBreakpoints.tablet} {
-    background-image: url(${heroTablet});
+    background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${heroTablet});
+    background-repeat:no-repeat;
+    background-size: cover;
+    background-position: center center;
   }
 
   @media ${responsiveBreakpoints.desktop} {
-    background-image: url(${heroDesktop});
+    background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${heroDesktop});
+    background-repeat:no-repeat;
+    background-size: cover;
+    background-position: center center;
   }
+`;
+
+export const HeroTitle = styled(Typography)`
+  color: ${AppColors.white};
 `;
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,14 +1,13 @@
-import { Typography } from '@mui/material';
 import { Header } from '../../components/templates';
 import { AnchorButton, PrimaryButton } from '../../styles';
 import { DASHBOARD_ROUTE } from '../RoutesConstants';
-import { Hero } from './LandingPage.styled';
+import { Hero, HeroTitle } from './LandingPage.styled';
 
 const LandingPage = () => (
   <>
     <Header />
     <Hero>
-      <Typography variant="body1">Take control of your finances</Typography>
+      <HeroTitle variant="h1">Take control of your finances</HeroTitle>
       <AnchorButton to={DASHBOARD_ROUTE}>
         <PrimaryButton variant="contained" size="large">Start now</PrimaryButton>
       </AnchorButton>
