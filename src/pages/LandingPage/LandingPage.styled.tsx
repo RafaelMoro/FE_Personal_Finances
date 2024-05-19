1d11de917a823abc8303fe9f82f9234941488deb
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-05-19 08:15:44 -0600

    Add app feature into landing page

--- src/pages/LandingPage/LandingPage.styled.tsx
+++ src/pages/LandingPage/LandingPage.styled.tsx
@@ -4,11 +4,13 @@ import { AppColors, responsiveBreakpoints } from '../../styles';
 import heroDesktop from '../../assets/hero-desktop-webp.webp';
 import heroMobile from '../../assets/hero-mobile-webp.webp';
 import heroTablet from '../../assets/hero-tablet-webp.webp';
+import { appTheme } from '../../styles/theme';
 
 export const Hero = styled.div`
+  height: 50rem;
+  margin-bottom: ${appTheme.spacing(5)};
   display: grid;
   place-items: center;
-  height: 50rem;
   background-image: url(${heroMobile});
   background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${heroMobile});
   background-repeat:no-repeat;
@@ -32,4 +34,24 @@ export const Hero = styled.div`
 
 export const HeroTitle = styled(Typography)`
   color: ${AppColors.white};
+`;
+
+export const FeatureBox = styled.article`
+  width: 100%;
+  margin: 0 auto;
+  max-width: 110rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10rem;
+`;
+
+export const FeaturePictureBox = styled.picture`
+  display: block;
+  img {
+    width: 40rem;
+    height: 100%;
+    border-radius: 1rem;
+    object-fit: contain;
+  }
 `;
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -2,7 +2,9 @@ import { Typography } from '@mui/material';
 import { Header } from '../../components/templates';
 import { AnchorButton, PrimaryButton } from '../../styles';
 import { DASHBOARD_ROUTE } from '../RoutesConstants';
-import { Hero, HeroTitle } from './LandingPage.styled';
+import {
+  FeatureBox, FeaturePictureBox, Hero, HeroTitle,
+} from './LandingPage.styled';
 
 import indebtedPeopleShot from '../../assets/shot-indebted-people.jpg';
 import indebtedPeopleShotWebp from '../../assets/shot-indebted-people.webp';
@@ -16,13 +18,18 @@ const LandingPage = () => (
         <PrimaryButton variant="contained" size="large">Start now</PrimaryButton>
       </AnchorButton>
     </Hero>
-    <article>
-      <Typography>Did you buy something with your friend? Have control on how money he owe you and how many he has paid.</Typography>
-      <picture>
+    <FeatureBox>
+      <Typography>
+        Did you buy something with your friend?
+        <br />
+        {' '}
+        Have control on how money he owe you and how many he has paid.
+      </Typography>
+      <FeaturePictureBox>
         <source srcSet={indebtedPeopleShotWebp} type="image/webp" />
         <img src={indebtedPeopleShot} alt="Indebted people feature" />
-      </picture>
-    </article>
+      </FeaturePictureBox>
+    </FeatureBox>
   </>
 );
 
