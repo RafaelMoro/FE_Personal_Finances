7f78f0be872b88f484d9d06d6d09094ef4b0d836
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-05-18 22:06:49 -0600

    Add feature text on landing page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,7 +1,11 @@
+import { Typography } from '@mui/material';
 import { Header } from '../../components/templates';
 import { AnchorButton, PrimaryButton } from '../../styles';
 import { DASHBOARD_ROUTE } from '../RoutesConstants';
 import { Hero, HeroTitle } from './LandingPage.styled';
+
+import indebtedPeopleShot from '../../assets/shot-indebted-people.jpg';
+import indebtedPeopleShotWebp from '../../assets/shot-indebted-people.webp';
 
 const LandingPage = () => (
   <>
@@ -12,6 +16,13 @@ const LandingPage = () => (
         <PrimaryButton variant="contained" size="large">Start now</PrimaryButton>
       </AnchorButton>
     </Hero>
+    <article>
+      <Typography>Did you buy something with your friend? Have control on how money he owe you and how many he has paid.</Typography>
+      <picture>
+        <source srcSet={indebtedPeopleShotWebp} type="image/webp" />
+        <img src={indebtedPeopleShot} alt="Indebted people feature" />
+      </picture>
+    </article>
   </>
 );
 
