d06db7a95fd322f3b2acd58d7175ea42d7dbed88
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-06-15 09:10:55 -0600

    Add hamburguer icon and drawer into header

--- src/components/templates/Header/Header.tsx
+++ src/components/templates/Header/Header.tsx
@@ -1,19 +1,21 @@
-import { IconButton, Typography } from '@mui/material';
+import { Drawer, IconButton, Typography } from '@mui/material';
+import { useState } from 'react';
 
+import { Link } from 'react-router-dom';
 import { useLogin } from '../../../hooks/useLogin';
+import { useSyncLoginInfo } from '../../../hooks/useSyncLoginInfo';
 import { useAppSelector } from '../../../redux/hooks';
+import { LOGIN_ROUTE, REGISTER_ROUTE } from '../../../pages/RoutesConstants';
+import { HeaderProps } from './interface';
+import { AppIcon } from '../../UI/Icons';
+import { BrandLogoName } from '../BrandLogoName';
 import {
   AnchorButton,
   FlexContainer,
   PrimaryButton,
   SecondaryButton,
 } from '../../../styles';
-import { AppIcon } from '../../UI/Icons';
 import { HeaderContainer, HeaderShadow } from './Header.styled';
-import { BrandLogoName } from '../BrandLogoName';
-import { useSyncLoginInfo } from '../../../hooks/useSyncLoginInfo';
-import { LOGIN_ROUTE, REGISTER_ROUTE } from '../../../pages/RoutesConstants';
-import { HeaderProps } from './interface';
 
 const Header = ({ isLandingPage = false }: HeaderProps) => {
   const { signOut } = useLogin();
@@ -21,31 +23,51 @@ const Header = ({ isLandingPage = false }: HeaderProps) => {
   const windowSize = useAppSelector((state) => state.userInterface.windowSize);
   const isMobile = windowSize === 'Mobile';
 
-  return (
-    <HeaderShadow isLandingPage={isLandingPage}>
-      <HeaderContainer>
-        <FlexContainer gap={2} alignItems="center">
-          <BrandLogoName isLandingPage={isLandingPage} />
-        </FlexContainer>
-        { (windowSize === 'Desktop' && hasSignedOn) && (<Typography variant="h3">Account management</Typography>) }
-        { (hasSignedOn) && (
-          <IconButton aria-label="sign-out-button" onClick={signOut}>
-            <AppIcon icon="LogOut" />
-          </IconButton>
-        ) }
-        { (!hasSignedOn && !isMobile) && (
-          <FlexContainer gap={3} justifyContent="center">
-            <AnchorButton to={LOGIN_ROUTE}>
-              <SecondaryButton variant="contained" size="medium">Sign in</SecondaryButton>
-            </AnchorButton>
-            <AnchorButton to={REGISTER_ROUTE}>
-              <PrimaryButton variant="contained" size="medium">Register</PrimaryButton>
-            </AnchorButton>
-          </FlexContainer>
+  const [openHamburguerDrawer, setOpenHamburguerDrawer] = useState(false);
+  const toggleHamburguerDrawer = () => setOpenHamburguerDrawer((prevState) => !prevState);
 
-        )}
-      </HeaderContainer>
-    </HeaderShadow>
+  return (
+    <>
+      <HeaderShadow isLandingPage={isLandingPage}>
+        <HeaderContainer>
+          <FlexContainer gap={2} alignItems="center">
+            <BrandLogoName isLandingPage={isLandingPage} />
+          </FlexContainer>
+          { (windowSize === 'Desktop' && hasSignedOn) && (<Typography variant="h3">Account management</Typography>) }
+          { (hasSignedOn) && (
+            <IconButton aria-label="sign-out-button" onClick={signOut}>
+              <AppIcon icon="LogOut" />
+            </IconButton>
+          ) }
+          { (!hasSignedOn && !isMobile) && (
+            <FlexContainer gap={3} justifyContent="center">
+              <AnchorButton to={LOGIN_ROUTE}>
+                <SecondaryButton variant="contained" size="medium">Sign in</SecondaryButton>
+              </AnchorButton>
+              <AnchorButton to={REGISTER_ROUTE}>
+                <PrimaryButton variant="contained" size="medium">Register</PrimaryButton>
+              </AnchorButton>
+            </FlexContainer>
+          )}
+          { (!hasSignedOn && isMobile) && (
+            <IconButton onClick={toggleHamburguerDrawer}>
+              <AppIcon icon="HamburguerMenu" />
+            </IconButton>
+          )}
+        </HeaderContainer>
+      </HeaderShadow>
+      <Drawer anchor="bottom" open={openHamburguerDrawer}>
+        <IconButton onClick={toggleHamburguerDrawer}>
+          <AppIcon icon="Close" />
+        </IconButton>
+        <Link to={LOGIN_ROUTE}>
+          <Typography>Log inn</Typography>
+        </Link>
+        <Link to={REGISTER_ROUTE}>
+          <Typography>Register</Typography>
+        </Link>
+      </Drawer>
+    </>
   );
 };
 
