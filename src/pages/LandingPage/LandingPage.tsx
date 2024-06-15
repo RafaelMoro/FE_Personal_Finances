1a2d7d321cd51d3ef49b074e0cd11d2216cd0962
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-06-15 09:10:12 -0600

    Add resize window utility fn into landing page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -13,40 +13,45 @@ import {
 
 import budgetMasterShot from '../../assets/app-feature-shots/budget-master-app-1.png';
 import { FeatureCard } from './FeatureCard';
+import { useResizeWindow } from '../../hooks/useResizeWindow';
 import {
   CONTROL_FEATURE_DESCRIPTION, CONTROL_FEATURE_TITLE, EASY_FEATURE_DESCRIPTION, EASY_FEATURE_TITLE,
 } from './constants';
 
-const LandingPage = () => (
-  <>
-    <HeaderHeroBox>
-      <Header isLandingPage />
-      <Hero>
-        <HeroTitle variant="h1">
-          Don&apos;t let your credit cards to be out of control
-        </HeroTitle>
-        <AnchorButtonHero to={DASHBOARD_ROUTE}>
-          <PrimaryButton variant="contained" size="large">Start now</PrimaryButton>
-        </AnchorButtonHero>
-      </Hero>
-    </HeaderHeroBox>
-    <AppDescription>
-      <AppDescriptionTitle align="center" variant="h3">
-        What is
-        {' '}
-        <span>Budget Master</span>
-        {' '}
-        ?
-      </AppDescriptionTitle>
-      <AppDescriptionBox>
-        <img src={budgetMasterShot} alt="Budget Master App Shot" />
-        <CardContainer>
-          <FeatureCard title={EASY_FEATURE_TITLE} body={EASY_FEATURE_DESCRIPTION} />
-          <FeatureCard title={CONTROL_FEATURE_TITLE} body={CONTROL_FEATURE_DESCRIPTION} />
-        </CardContainer>
-      </AppDescriptionBox>
-    </AppDescription>
-  </>
-);
+const LandingPage = () => {
+  useResizeWindow();
+
+  return (
+    <>
+      <HeaderHeroBox>
+        <Header isLandingPage />
+        <Hero>
+          <HeroTitle variant="h1">
+            Don&apos;t let your credit cards to be out of control
+          </HeroTitle>
+          <AnchorButtonHero to={DASHBOARD_ROUTE}>
+            <PrimaryButton variant="contained" size="large">Start now</PrimaryButton>
+          </AnchorButtonHero>
+        </Hero>
+      </HeaderHeroBox>
+      <AppDescription>
+        <AppDescriptionTitle align="center" variant="h3">
+          What is
+          {' '}
+          <span>Budget Master</span>
+          {' '}
+          ?
+        </AppDescriptionTitle>
+        <AppDescriptionBox>
+          <img src={budgetMasterShot} alt="Budget Master App Shot" />
+          <CardContainer>
+            <FeatureCard title={EASY_FEATURE_TITLE} body={EASY_FEATURE_DESCRIPTION} />
+            <FeatureCard title={CONTROL_FEATURE_TITLE} body={CONTROL_FEATURE_DESCRIPTION} />
+          </CardContainer>
+        </AppDescriptionBox>
+      </AppDescription>
+    </>
+  );
+};
 
 export { LandingPage };
