aeabf0c38d3902a2a752b5e0e142eb9722d56a3c
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-06-17 13:20:45 -0600

    Add methods to add user and load guest user in useGuestUser hook

--- src/hooks/useGuestUser/useGuestUser.tsx
+++ src/hooks/useGuestUser/useGuestUser.tsx
@@ -1,4 +1,33 @@
-const useGuesUser = () => {
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { updateAccounts } from '../../redux/slices/Accounts/accounts.slice';
+import { signOn } from '../../redux/slices/User/user.slice';
+import { addToLocalStorage } from '../../utils';
+import { useGuestUserMocks } from './useGuestUserMocks';
+
+const useGuestUser = () => {
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user.userInfo);
+  const {
+    recordsAmericanExpress, recordsDebitAccount, accounts, guestUser,
+  } = useGuestUserMocks();
+
+  const addGuestUser = () => {
+    dispatch(signOn(guestUser));
+    addToLocalStorage(guestUser);
+    dispatch(updateAccounts(accounts));
+    addToLocalStorage({ accounts });
+  };
+
+  const loadGuestUser = () => {
+    dispatch(signOn(guestUser));
+    dispatch(updateAccounts(accounts));
+    // Load records
+  };
+
+  return {
+    addGuestUser,
+    loadGuestUser,
+  };
 };
 
-export { useGuesUser };
+export { useGuestUser };
