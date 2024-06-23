7cd9173f1e98a6e35ab8fb59e8640884c7f5fefc
Rafael Moro G <rafael.moro.galindo@gmail.com>
2024-06-23 12:56:31 -0600

    Refactor function getAgeLocalRecord to getAgeLocalRecord

--- src/hooks/useRecords/utils.ts
+++ src/hooks/useRecords/utils.ts
@@ -1,18 +1,20 @@
 import { CreateExpenseValues, CreateIncomeValues } from '../../components/UI/Records/interface';
 import { AbbreviatedMonthsType } from '../../globalInterface';
+import { RecordAgeStatusKey } from '../../utils/LocalStorage/interface';
 
 export function isCreateExpense(values: CreateExpenseValues | CreateIncomeValues): values is CreateExpenseValues {
   return (values as CreateExpenseValues).isPaid !== undefined;
 }
 
-export const getAge = (
+export const getAgeLocalRecord = (
   { month, lastMonth, currentMonth }
   : { month: AbbreviatedMonthsType, lastMonth: AbbreviatedMonthsType, currentMonth: AbbreviatedMonthsType },
-) => {
-  const isLastMonth = lastMonth === month;
-  const isCurrentMonth = currentMonth === month;
-  return {
-    isLastMonth,
-    isCurrentMonth,
-  };
+): RecordAgeStatusKey => {
+  if (currentMonth === month) {
+    return 'currentMonth';
+  }
+  if (lastMonth === month) {
+    return 'lastMonth';
+  }
+  return 'olderRecords';
 };
