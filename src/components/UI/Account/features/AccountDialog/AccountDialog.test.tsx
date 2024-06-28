ac4fde73c7a97afa20b0bd6b8d1fa8a55a550ab5
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-06-28 16:16:31 -0600

    chore: Add lazy loading for images in AccountDialog test and update npm dependencies

--- src/components/UI/Account/features/AccountDialog/AccountDialog.test.tsx
+++ src/components/UI/Account/features/AccountDialog/AccountDialog.test.tsx
@@ -1,7 +1,39 @@
-import { screen, waitFor } from '@testing-library/react';
+import { screen, waitFor, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import fetchMock from 'jest-fetch-mock';
+
 import { AccountDialog } from './AccountDialog';
 import { renderWithProviders } from '../../../../../tests/CustomWrapperRedux';
+
+const successfulResponse = {
+  version: '2.0.0',
+  success: true,
+  message: null,
+  data: {
+    account: {
+      _id: '123',
+      title: 'Bank account',
+      accountType: 'Credit',
+      backgroundColor: 'Teal',
+      color: 'White',
+      amount: 499,
+      sub: '123-456',
+      __v: 0,
+    },
+  },
+  error: null,
+};
+const unsuccessfulResponse = {
+  version: '2.0.0',
+  success: false,
+  message: null,
+  data: null,
+  error: {
+    message: 'Unauthorized',
+    statusCode: 401,
+  },
+};
+afterEach(cleanup);
 
 beforeEach(() => {
   // having console error because of formik.
@@ -54,6 +86,48 @@ describe('<AccountDialog />', () => {
     await waitFor(() => {
       expect(screen.getByText(/the title of your account is required\./i)).toBeInTheDocument();
       expect(screen.getByText(/the initial amount of your account is required\./i)).toBeInTheDocument();
+    });
+  });
+});
+
+describe('AccountDialog creation account', () => {
+  beforeEach(() => {
+    fetchMock.resetMocks();
+    jest.clearAllMocks();
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
+  test('Create account with success', async () => {
+    fetchMock.once(JSON.stringify(successfulResponse));
+    const onClose = jest.fn();
+    renderWithProviders(
+      <AccountDialog
+        open
+        onClose={onClose}
+        accountAction="Create"
+        account={null}
+      />,
+      { preloadedState: {} },
+    );
+
+    const accountTitleTextBox = screen.getByRole('textbox', {
+      name: /account title/i,
+    });
+    const amountTextBox = screen.getByRole('textbox', {
+      name: /amount/i,
+    });
+    const createAccountByutton = screen.getByRole('button', {
+      name: /create account/i,
+    });
+
+    userEvent.type(accountTitleTextBox, 'Bank account');
+    userEvent.type(amountTextBox, '499');
+
+    userEvent.click(createAccountByutton);
+
+    await waitFor(() => {
+      expect(fetchMock).toHaveBeenCalled();
+      expect(onClose).toHaveBeenCalled();
     });
   });
 });
