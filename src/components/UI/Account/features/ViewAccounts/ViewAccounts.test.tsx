c68da5f900a09b3607f024e39401be8c52bccb76
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-06-29 11:45:45 -0600

    Add test show accounts on mobile

--- src/components/UI/Account/features/ViewAccounts/ViewAccounts.test.tsx
+++ src/components/UI/Account/features/ViewAccounts/ViewAccounts.test.tsx
@@ -1,6 +1,10 @@
+import fetchMock from 'jest-fetch-mock';
+import { waitFor, screen } from '@testing-library/react';
 import { UserInitialState } from '../../../../../redux/slices/User/interface';
 import { renderWithProviders } from '../../../../../tests/CustomWrapperRedux';
+import { AccountActions } from '../../Account.interface';
 import { ViewAccounts } from './ViewAccounts';
+import { mockedAccounts } from '../../Account.mocks';
 
 const userInitialState: UserInitialState = {
   userInfo: {
@@ -16,8 +20,50 @@ const userInitialState: UserInitialState = {
   },
 };
 
+const accountsActions: AccountActions = {
+  accountAction: 'Create',
+  openAccountModal: false,
+  openChangeToOtherAccountModal: false,
+  modifyAccount: null,
+  openDeleteAccountModal: false,
+  accountToBeDeleted: { current: { accountId: '', accountName: '' } },
+  handleCloseAccountModal: jest.fn(),
+  handleOpenCreateAccount: jest.fn(),
+  handleOpenModifyAccount: jest.fn(),
+  toggleChangeOtherAccountModal: jest.fn(),
+  handleCloseDeleteAccount: jest.fn(),
+  handleOpenDeleteAccount: jest.fn(),
+};
+
+const response = {
+  data: {
+    accounts: mockedAccounts,
+  },
+  error: null,
+  message: null,
+  success: true,
+  version: '2.0.0',
+};
+
 describe('ViewAccounts', () => {
-  test('Show accounts', () => {
-    renderWithProviders(<ViewAccounts hide={null} />, { preloadedState: { user: userInitialState } });
+  beforeEach(() => {
+    fetchMock.resetMocks();
+    jest.clearAllMocks();
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
+  // It will show only the first one
+  test('Show accounts on mobile', async () => {
+    fetchMock.once(JSON.stringify(response));
+    renderWithProviders(
+      <ViewAccounts hide={null} accountsActions={accountsActions} />,
+      { preloadedState: { user: userInitialState } },
+    );
+
+    await waitFor(() => {
+      expect(fetchMock).toHaveBeenCalled();
+      expect(screen.getByText('Citibanamex Debit')).toBeInTheDocument();
+      expect(screen.getByText('$8,246.41')).toBeInTheDocument();
+    });
   });
 });
