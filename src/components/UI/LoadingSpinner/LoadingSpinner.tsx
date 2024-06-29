d9536b49abe06d21fc4b1493a7a0aa5bcac7c9ec
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-06-29 12:55:14 -0600

    Add loading spinner test

--- src/components/UI/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/UI/LoadingSpinner/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import { AppColors } from '../../../styles';
 import { Spinner } from './LoadingSpinner.styled';
 
 const LoadingSpinner = ({ color = AppColors.white, borderSize = '0.5' }: LoadingSpinnerProps) => (
-  <Spinner color={color} borderSize={borderSize} />
+  <Spinner data-testid="loading-spinner" color={color} borderSize={borderSize} />
 );
 
 export { LoadingSpinner };
