712697f42a8050089d96e1d7f7a63c3d6e0a861d
Rafael Moro G <rafael.moro.galindo@gmail.com>
2024-06-30 14:32:32 -0600

    Add Show an error if the input is empty and the button is clicked test

--- src/components/UI/Records/features/AddChip/AddChip.test.tsx
+++ src/components/UI/Records/features/AddChip/AddChip.test.tsx
@@ -21,3 +21,21 @@ test('Show an input with placeholder add tag and add tag button', () => {
   expect(screen.getByRole('button', { name: /add tag/i })).toBeInTheDocument();
   expect(screen.getByText(/no tags added/i)).toBeInTheDocument();
 });
+
+test('Show an error if the input is empty and the button is clicked', async () => {
+  const tags: string[] = [];
+  const mockUpdateData = jest.fn((newTags) => tags.push(newTags));
+  render(
+    <AddChip
+      name="tag"
+      label="Tag"
+      action="Add Tag"
+      chipsData={tags}
+      updateData={mockUpdateData}
+    />,
+  );
+  const button = screen.getByRole('button', { name: /add tag/i });
+  userEvent.click(button);
+
+  expect(await screen.findByText(/Tag cannot be added empty/i)).toBeInTheDocument();
+});
