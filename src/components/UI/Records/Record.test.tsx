974389191b6da52d889e3d1a01cd3b6b0120c2c1
Rafael Moro G <rafael.moro.galindo@gmail.com>
2024-06-30 13:23:51 -0600

    Add Show record with tags and budgets for Desktop test

--- src/components/UI/Records/Record.test.tsx
+++ src/components/UI/Records/Record.test.tsx
@@ -449,6 +449,24 @@ describe('<Records />', () => {
       expect(screen.getByText(/no tags/i)).toBeInTheDocument();
       expect(screen.getByText(/transfer/i)).toBeInTheDocument();
     });
+
+    test('Show record with tags and budgets for Desktop', () => {
+      const backgroundColor = 'green';
+      const history = createMemoryHistory();
+      const expense = getMockExpense({ tag: ['first tag'], budgets: ['first budget'] });
+      renderWithProviders(
+        <Router location={history.location} navigator={history}>
+          <Record
+            record={expense}
+            backgroundColor={backgroundColor}
+          />
+        </Router>,
+        { preloadedState: { userInterface: initialUserInterfaceState } },
+      );
+
+      expect(screen.getByText(/first tag/i)).toBeInTheDocument();
+      expect(screen.getByText(/first budget/i)).toBeInTheDocument();
+    });
   });
 
   test('Show Records Paid verbiage with transfer income record', () => {
