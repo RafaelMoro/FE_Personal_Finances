277f8cdb617dadd5de1a9d7bc043b78bd9ebcef5
Rafael Moro G <rafael.moro.galindo@gmail.com>
2024-06-30 20:18:48 -0600

    test: Add test for adding a tag and updating the array

--- src/components/UI/Records/features/AddChip/AddChip.test.tsx
+++ src/components/UI/Records/features/AddChip/AddChip.test.tsx
@@ -1,5 +1,5 @@
 import userEvent from '@testing-library/user-event';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { AddChip } from './AddChip';
 
 beforeEach(() => {
@@ -20,6 +20,32 @@ test('Show an input with placeholder add tag and add tag button', () => {
 
   expect(screen.getByRole('button', { name: /add tag/i })).toBeInTheDocument();
   expect(screen.getByText(/no tags added/i)).toBeInTheDocument();
+});
+
+test('Add a tag and the array has changed', async () => {
+  const tags: string[] = [];
+  const mockUpdateData = jest.fn((newTags) => tags.push(newTags));
+  render(
+    <AddChip
+      name="tag"
+      label="Tag"
+      action="Add Tag"
+      chipsData={tags}
+      updateData={mockUpdateData}
+    />,
+  );
+  expect(tags.length).toBe(0);
+
+  const input = screen.getByRole('textbox', {
+    name: /tag$/i,
+  });
+  const button = screen.getByRole('button', { name: /add tag/i });
+  userEvent.type(input, 'New tag');
+  userEvent.click(button);
+
+  await waitFor(() => {
+    expect(tags.length).toBe(1);
+  });
 });
 
 test('Show an error if the input is empty and the button is clicked', async () => {
