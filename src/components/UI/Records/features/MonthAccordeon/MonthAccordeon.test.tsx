4dd66700320966f3d5ca89e0e629f943c84625ed
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-01 18:07:36 -0600

    Add test: Click on month accordeon and show opened

--- src/components/UI/Records/features/MonthAccordeon/MonthAccordeon.test.tsx
+++ src/components/UI/Records/features/MonthAccordeon/MonthAccordeon.test.tsx
@@ -1,7 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { MonthAccordeon } from './MonthAccordeon';
 
 describe('<MonthAccordeon />', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
   const title = 'January';
   test('Show month accordeon', () => {
     render(
@@ -23,5 +28,21 @@ describe('<MonthAccordeon />', () => {
     );
 
     expect(screen.getByTestId('ExpandLessIcon')).toBeInTheDocument();
+  });
+
+  test('Click on month accordeon and show opened.', async () => {
+    render(
+      <MonthAccordeon title={title} color="blue" accountId="1">
+        <div>Child Content</div>
+      </MonthAccordeon>,
+    );
+
+    expect(screen.getByTestId('ExpandMoreIcon')).toBeInTheDocument();
+    expect(screen.queryByTestId('ExpandLessIcon')).not.toBeInTheDocument();
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    expect(await screen.findByTestId('ExpandLessIcon')).toBeInTheDocument();
+    expect(screen.queryByTestId('ExpandMoreIcon')).not.toBeInTheDocument();
   });
 });
