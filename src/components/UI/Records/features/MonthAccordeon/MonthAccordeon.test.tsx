477a3fff40f829e63705cca339cf43ac998b4b1b
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-07-01 18:05:13 -0600

    test: Add month accordeon opened test case

--- src/components/UI/Records/features/MonthAccordeon/MonthAccordeon.test.tsx
+++ src/components/UI/Records/features/MonthAccordeon/MonthAccordeon.test.tsx
@@ -11,6 +11,17 @@ describe('<MonthAccordeon />', () => {
     );
 
     expect(screen.getByText(title)).toBeInTheDocument();
+    expect(screen.getByTestId('ExpandMoreIcon')).toBeInTheDocument();
     expect(screen.queryByText('Child Content')).not.toBeInTheDocument();
+  });
+
+  test('Show month accordeon opened', () => {
+    render(
+      <MonthAccordeon title={title} color="blue" accountId="1" opened>
+        <div>Child Content</div>
+      </MonthAccordeon>,
+    );
+
+    expect(screen.getByTestId('ExpandLessIcon')).toBeInTheDocument();
   });
 });
